9a0e786a0ecdf9abaef5179e1488425fe6e90f5c
hongxeob <sjun2918@naver.com>
2024-11-24 16:34:45 +0900

    feat: User 엔티티 설정

---
+++ src/main/java/org/example/wanted/user/domain/User.java
@@ -0,0 +1,30 @@
+package org.example.wanted.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.wanted.base.BaseTimeEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+@Getter
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+    private String phone;
+
+    @Builder
+
+    public User(String email, String name, String phone) {
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/org/example/wanted/user/domain/UserRepository.java
@@ -0,0 +1,6 @@
+package org.example.wanted.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
