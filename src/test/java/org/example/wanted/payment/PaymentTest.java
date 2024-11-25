e08dc2b25b82bbc2f1667ffeba0601f3d223da93
hongxeob <sjun2918@naver.com>
2024-11-25 10:55:34 +0900

    style: 개행 변경

--- src/test/java/org/example/wanted/payment/PaymentTest.java
+++ src/test/java/org/example/wanted/payment/PaymentTest.java
@@ -1,9 +1,5 @@
 package org.example.wanted.payment;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.math.BigDecimal;
-
 import org.example.wanted.order.domain.Order;
 import org.example.wanted.payment.domain.Payment;
 import org.example.wanted.payment.domain.PaymentMethod;
@@ -13,85 +9,89 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PaymentTest {
 
-	private Order order;
-	private User user;
+    private Order order;
+    private User user;
 
-	@BeforeEach
-	void setUp() {
-		user = genereateUser();
-		order = genereateOrder(user);
-	}
+    @BeforeEach
+    void setUp() {
+        user = genereateUser();
+        order = genereateOrder(user);
+    }
 
-	@Test
-	@DisplayName("결제를 생성할 수 있다")
-	void createPaymentSuccessTest() throws Exception {
+    @Test
+    @DisplayName("결제를 생성할 수 있다")
+    void createPaymentSuccessTest() throws Exception {
 
-		//given -> when
-		Payment payment = genereatePayment(order);
+        //given -> when
+        Payment payment = genereatePayment(order);
 
-		//then
-		assertThat(payment).isNotNull();
-		assertThat(payment.getPaymentMethod()).isEqualTo(PaymentMethod.CARD);
-		assertThat(payment.getOrder()).isEqualTo(order);
-		assertThat(payment.getCancelReason()).isNull();
-	}
+        //then
+        assertThat(payment).isNotNull();
+        assertThat(payment.getPaymentMethod()).isEqualTo(PaymentMethod.CARD);
+        assertThat(payment.getOrder()).isEqualTo(order);
+        assertThat(payment.getCancelReason()).isNull();
+    }
 
-	@Test
-	@DisplayName("결제를 지불할 수 있다")
-	void paySuccessTest() throws Exception {
+    @Test
+    @DisplayName("결제를 지불할 수 있다")
+    void paySuccessTest() throws Exception {
 
-		//given
-		Payment payment = genereatePayment(order);
+        //given
+        Payment payment = genereatePayment(order);
 
-		//when
-		payment.pay();
+        //when
+        payment.pay();
 
-		//then
-		assertThat(payment.getStatus()).isEqualTo(PaymentStatus.COMPLETED);
-		assertThat(payment.getPaidAt()).isNotNull();
-	}
+        //then
+        assertThat(payment.getStatus()).isEqualTo(PaymentStatus.COMPLETED);
+        assertThat(payment.getPaidAt()).isNotNull();
+    }
 
-	@Test
-	@DisplayName("결제를 취소할 수 있다")
-	void cancelSuccessTest() throws Exception {
+    @Test
+    @DisplayName("결제를 취소할 수 있다")
+    void cancelSuccessTest() throws Exception {
 
-		//given
-		Payment payment = genereatePayment(order);
+        //given
+        Payment payment = genereatePayment(order);
 
-		//when
-		payment.cancel("reason");
+        //when
+        payment.cancel("reason");
 
-		//then
-		assertThat(payment.getCancelReason()).isEqualTo("reason");
-		assertThat(payment.getCancelledAt()).isNotNull();
-		assertThat(payment.getStatus()).isEqualTo(PaymentStatus.CANCELLED);
-	}
+        //then
+        assertThat(payment.getCancelReason()).isEqualTo("reason");
+        assertThat(payment.getCancelledAt()).isNotNull();
+        assertThat(payment.getStatus()).isEqualTo(PaymentStatus.CANCELLED);
+    }
 
-	private static Payment genereatePayment(Order order) {
-		return Payment.builder()
-			.order(order)
-			.paymentMethod(PaymentMethod.CARD)
-			.build();
-	}
+    private static Payment genereatePayment(Order order) {
+        return Payment.builder()
+                .order(order)
+                .paymentMethod(PaymentMethod.CARD)
+                .build();
+    }
 
-	private static Order genereateOrder(User user) {
-		return Order.builder()
-			.user(user)
-			.totalAmount(BigDecimal.valueOf(100))
-			.build();
-	}
+    private static Order genereateOrder(User user) {
+        return Order.builder()
+                .user(user)
+                .totalAmount(BigDecimal.valueOf(100))
+                .build();
+    }
 
-	private static User genereateUser() {
-		String email = "email@email.com";
-		String name = "name";
-		String phone = "010-1234-1234";
+    private static User genereateUser() {
+        String email = "email@email.com";
+        String name = "name";
+        String phone = "010-1234-1234";
 
-		return User.builder()
-			.email(email)
-			.name(name)
-			.phone(phone)
-			.build();
-	}
+        return User.builder()
+                .email(email)
+                .name(name)
+                .phone(phone)
+                .build();
+    }
 }
--- src/test/java/org/example/wanted/user/domain/UserTest.java
+++ src/test/java/org/example/wanted/user/domain/UserTest.java
@@ -1,32 +1,32 @@
 package org.example.wanted.user.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class UserTest {
 
-	@Test
-	@DisplayName("유저를 생성할 수 있다")
-	void createSuccessTest() throws Exception {
+    @Test
+    @DisplayName("유저를 생성할 수 있다")
+    void createSuccessTest() throws Exception {
 
-		//given
-		String email = "email@email.com";
-		String name = "name";
-		String phone = "010-1234-1234";
+        //given
+        String email = "email@email.com";
+        String name = "name";
+        String phone = "010-1234-1234";
 
-		//when
-		User user = User.builder()
-			.email(email)
-			.name(name)
-			.phone(phone)
-			.build();
+        //when
+        User user = User.builder()
+                .email(email)
+                .name(name)
+                .phone(phone)
+                .build();
 
-		//then
-		assertThat(user).isNotNull();
-		assertThat(user.getEmail()).isEqualTo(email);
-		assertThat(user.getName()).isEqualTo(name);
-		assertThat(user.getPhone()).isEqualTo(phone);
-	}
+        //then
+        assertThat(user).isNotNull();
+        assertThat(user.getEmail()).isEqualTo(email);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getPhone()).isEqualTo(phone);
+    }
 }
